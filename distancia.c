af58f2d9b73dece452ed419afdd0d9fcffe87797
Rpablo <rpablooliveira2@gmail.com>
2023-10-10 11:18:22 -0300

    calcula a distancia e mostra o resultado

--- distancia.c
+++ distancia.c
@@ -12,4 +12,37 @@ int main(){
 	// definição das variáveis
 	int x1, x2, y1, y2;
 	float distancia;
+	
+	// Leitura das coordenadas dos pontos
+	printf("Informe as coordenadas para o primeiro ponto: ");
+	scanf("%i %i", &x1, &y1);
+	printf("Informe as coordenadas para o segundo ponto: ");
+	scanf("%i %i", &x2, &y2);
+	
+	// Calculo da distância entre os pontos
+	distancia = calculaDistancia(x1,y1,x2,y2);
+	
+	// Mostra o resultado
+	printf("Distancia: %f", distancia);
+}
+#include <stdio.h>
+#include <math.h>
+
+// função que calcula a distancia
+float calculaDistancia(int x1, int y1, int x2, int y2){
+	return sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+}
+
+// main
+int main(){
+	
+	// definição das variáveis
+	int x1, x2, y1, y2;
+	float distancia;
+	
+	// Leitura das coordenadas dos pontos
+	printf("Informe as coordenadas para o primeiro ponto: ");
+	scanf("%i %i", &x1, &y1);
+	printf("Informe as coordenadas para o primeiro ponto: ");
+	scanf("%i %i", &x2, &y2);
 }
