a46d5d83cd47837e4e87c559b411d935d6740fa9
Rpablo-7 <146092990+Rpablo-7@users.noreply.github.com>
2023-10-10 12:04:29 -0300

    Correção

--- distancia.c
+++ distancia.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-// função que calcula a distancia
+// funï¿½ï¿½o que calcula a distancia
 float calculaDistancia(int x1, int y1, int x2, int y2){
 	return sqrt(pow(x2-x1,2) + pow(y2-y1,2));
 }
@@ -9,7 +9,7 @@ float calculaDistancia(int x1, int y1, int x2, int y2){
 // main
 int main(){
 	
-	// definição das variáveis
+	// definiï¿½ï¿½o das variï¿½veis
 	int x1, x2, y1, y2;
 	float distancia;
 	
@@ -19,30 +19,9 @@ int main(){
 	printf("Informe as coordenadas para o segundo ponto: ");
 	scanf("%i %i", &x2, &y2);
 	
-	// Calculo da distância entre os pontos
+	// Calculo da distï¿½ncia entre os pontos
 	distancia = calculaDistancia(x1,y1,x2,y2);
 	
 	// Mostra o resultado
 	printf("Distancia: %f", distancia);
-}
-#include <stdio.h>
-#include <math.h>
-
-// função que calcula a distancia
-float calculaDistancia(int x1, int y1, int x2, int y2){
-	return sqrt(pow(x2-x1,2) + pow(y2-y1,2));
-}
-
-// main
-int main(){
-	
-	// definição das variáveis
-	int x1, x2, y1, y2;
-	float distancia;
-	
-	// Leitura das coordenadas dos pontos
-	printf("Informe as coordenadas para o primeiro ponto: ");
-	scanf("%i %i", &x1, &y1);
-	printf("Informe as coordenadas para o primeiro ponto: ");
-	scanf("%i %i", &x2, &y2);
 }
